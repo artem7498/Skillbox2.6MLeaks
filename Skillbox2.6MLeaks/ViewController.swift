e342a6d343fcf9abfc0643a37800ab0474bdc8e3
Artem Akopian <artem7498@gmail.com>
2020-11-23 14:57:53 +1000

    Initial Commit

---
+++ Skillbox2.6MLeaks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox2.6MLeaks
+//
+//  Created by Артём on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
